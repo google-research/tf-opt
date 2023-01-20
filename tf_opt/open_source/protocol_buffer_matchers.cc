27e8bcdc96ab102b4b6378982462139a2d8bce61
Mike Kruskal <mkruskal@google.com>
2023-01-19 18:47:27 -0800

    Internal change
    PiperOrigin-RevId: 503320992

--- tf_opt/open_source/protocol_buffer_matchers.cc
+++ tf_opt/open_source/protocol_buffer_matchers.cc
@@ -60,14 +60,14 @@ class StringErrorCollector : public google::protobuf::io::ErrorCollector {
   explicit StringErrorCollector(std::string* error_text)
       : error_text_(error_text) {}
 
-  void AddError(int line, int column, const std::string& message) override {
+  void RecordError(int line, int column, absl::string_view message) override {
     absl::SubstituteAndAppend(error_text_, "$0($1): $2\n", line, column,
-                              message.c_str());
+                              message);
   }
 
-  void AddWarning(int line, int column, const std::string& message) override {
+  void RecordWarning(int line, int column, absl::string_view message) override {
     absl::SubstituteAndAppend(error_text_, "$0($1): $2\n", line, column,
-                              message.c_str());
+                              message);
   }
 
  private:
